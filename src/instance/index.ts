b103a4b477a7f6d415c9280fc0d54df22ba84e49
tflin <tflins@163.com>
2020-10-23 15:17:31 +0800

    fix: 优化翻页函数, 导航按钮点击功能

--- src/instance/index.ts
+++ src/instance/index.ts
@@ -1,4 +1,7 @@
-import { $, $$, getWheelDelta, throttle, addClass, removeClass } from '../utils'
+import { $, $$, getWheelDelta, throttle, addClass, removeClass, hasClass } from '../utils'
+
+// 动画时长
+const ANIMATION_TIME: number = 1000
 
 export default class TFullPage {
 
@@ -8,14 +11,14 @@ export default class TFullPage {
   readonly viewHeight: number
   // 滚动总页数
   readonly pageCount: number
+  // 导航按钮
+  readonly $navBtns: HTMLElement[] = []
   // 当前定位高度
   curPosition: number
-  // 当前定位
+  // 当前定位索引
   curIndex: number
   // 是否处于滚动的过程中
   isScrolling: boolean = false
-  // 导航按钮
-  $navBtns: HTMLElement[] = []
 
   constructor() {
     this.$container = $('.t-full-page')
@@ -43,29 +46,28 @@ export default class TFullPage {
   private down(): void {
     if (this.curIndex <= 0) return
     console.log('向下滚动')
-    this.curPosition += this.viewHeight
     this.curIndex--
-    this.turnPage(this.curPosition)
+    this.goToPage(this.curIndex)
   }
 
   // 向上翻页
   private up(): void {
     if (this.curIndex >= this.pageCount - 1) return
     console.log('向上滚动')
-    this.curPosition -= this.viewHeight
     this.curIndex++
-    this.turnPage(this.curPosition)
+    this.goToPage(this.curIndex)
   }
 
   // 翻页
-  private turnPage(height: number): void {
+  private goToPage(index: number): void {
     this.isScrolling = true
-    this.$container.style.top = `${height}px`
+    this.curPosition = -(this.viewHeight * index)
+    this.$container.style.top = `${this.curPosition}px`
     this.setCurActiveBtn(this.curIndex)
 
     setTimeout((): void => {
       this.isScrolling = false
-    }, 1000)
+    }, ANIMATION_TIME)
   }
 
   // 鼠标滚动时
@@ -85,6 +87,7 @@ export default class TFullPage {
 
     for (let i: number = 0; i < this.pageCount; i++) {
       const $navItem: HTMLElement = document.createElement('li')
+      $navItem.setAttribute('page-index', i.toString())
       addClass($navItem, 'nav-list-item')
       this.$navBtns.push($navItem)
       $navUl.appendChild($navItem)
@@ -94,12 +97,24 @@ export default class TFullPage {
 
     $nav.appendChild($navUl)
     this.$container.appendChild($nav)
+    $navUl.addEventListener('click', e => {
+      this.onClickNavBtn(e)
+    })
   }
 
   // 设置当前激活按钮
   private setCurActiveBtn(index: number): void {
     this.$navBtns.forEach($navItem => removeClass($navItem, 'active'))
     addClass(this.$navBtns[index], 'active')
+  }
 
+  // 点击导航按钮
+  private onClickNavBtn(event: MouseEvent) {
+    const target = event.target
+    if (hasClass(<HTMLElement>target, 'nav-list-item')) {
+      this.curIndex = ~~(target as HTMLElement).getAttribute('page-index')
+      this.goToPage(this.curIndex)
+    }
+    event.stopPropagation()
   }
 }
